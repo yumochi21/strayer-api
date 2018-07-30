2cf6b1372d372e2b940475280d70ec86b7558a48
yumochi21 <yumochi21@gmail.com>
2018-07-30 17:01:01 +0900

    [Update]リンクのサンプルを取得するAPIを作成

--- routes/links.js
+++ routes/links.js
@@ -2,24 +2,50 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/:code', function(req, res, next) {
-  var param = getLink(req.params.code);
+  var param = createLinks(req.params.code);
   res.header('Content-Type', 'application/json; charset=utf-8')
   res.send(param);
 });
 
-const getLink = (code) => {
-  return {
-    code: code,
-    links: [
-      {code: '00001', category: '0001', label: 'Java1', url: 'https://google.co.jp/'},
-      {code: '00002', category: '0001', label: 'Java2', url: 'https://google.co.jp/'},
-      {code: '00003', category: '0001', label: 'Java3', url: 'https://google.co.jp/'},
-      {code: '00004', category: '0001', label: 'Java4', url: 'https://google.co.jp/'},
-      {code: '00005', category: '0001', label: 'Java5', url: 'https://google.co.jp/'},
-      {code: '00006', category: '0001', label: 'Java6', url: 'https://google.co.jp/'},
-      {code: '00007', category: '0001', label: 'Java7', url: 'https://google.co.jp/'}
-    ]
+const createLinks = (code) => {
+  let label = ''
+  switch (code) {
+    case '001':
+      label = 'Java'
+      break
+    case '002':
+      label = 'CSS'
+      break
+    case '003':
+      label = 'HTML'
+      break
+    case '004':
+      label = 'C#'
+      break
+    case '005':
+      label = '機会学習'
+      break
+    case '006':
+      label = 'ビジネス'
+      break
+    case '007':
+      label = 'サンプル'
+      break
+    case '008':
+      label = 'テスト'
+      break
   }
+  let ret = []
+  for (let i = 1; i < 9; i++) {
+    let link = {
+      code: '00000' + i,
+      category: code,
+      label: label + i,
+      url: 'https://google.co.jp/'
+    }
+    ret.push(link)
+  }
+  return ret
 }
 
 module.exports = router;
